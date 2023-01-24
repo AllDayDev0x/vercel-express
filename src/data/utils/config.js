dc72d68a65c5df474361941734de69d2a1804ed7
star0713 <whitestar0713@gmail.com>
2023-01-24 13:35:50 -0800

    first commit

---
+++ src/data/utils/config.js
@@ -0,0 +1,12 @@
+module.exports = function () {
+    return {
+        etherscan: {
+            endpoint: "https://api.etherscan.io/api",
+            apikey: "CCIKGKR5PSF6ZBQVCZYHYUTSX97U95TUVA"
+        },
+        opensea: {
+            endpoint: "https://api.opensea.io/api/v1",
+            apikey: "a3a0dbfccb1f49dbaa9ac06cbcf9b9f2",
+        }
+    }
+};
